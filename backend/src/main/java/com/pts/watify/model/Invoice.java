6d7adb566bbed1853fc36ec5a1da1c6a935a742d
Igor Kuzevanov <ikuzevanov@profinit.eu>
2021-10-16 16:54:53 +0200

    Id generation

--- backend/src/main/java/com/pts/watify/model/Invoice.java
+++ backend/src/main/java/com/pts/watify/model/Invoice.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 @Entity
 public class Invoice {
     @Id
+    @GeneratedValue
     Long id;
     @Column
     String title;
--- backend/src/main/java/com/pts/watify/model/User.java
+++ backend/src/main/java/com/pts/watify/model/User.java
@@ -1,17 +1,21 @@
 package com.pts.watify.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity(name = "watify_user")
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
+    @GeneratedValue
     Long id;
     @Column
     String name;
