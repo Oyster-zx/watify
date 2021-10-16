bc6501e4f18f8c3de0073eb03035838a5828b62b
Igor Kuzevanov <ikuzevanov@profinit.eu>
2021-10-16 11:35:33 +0200

    Init

---
+++ backend/src/main/java/com/pts/watify/WatifyApplication.java
@@ -0,0 +1,13 @@
+package com.pts.watify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatifyApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/pts/watify/WatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pts.watify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
